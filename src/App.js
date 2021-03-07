8c81023713a39a8cf214ac9219318bb3276e278a
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-03-07 01:49:35 -0500

    .
    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+//import { Frame } from "framer";
+import {  motion } from 'framer-motion';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, rotate: 360 }} transition={{ duration: 20 }} >
+          <img src={logo} className="App-logo" alt="logo" />
+        </motion.div>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -17,7 +21,12 @@ function App() {
         >
           Learn React
         </a>
-        <p>KEVIN ARELLANO - 2021</p>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100, y: 100 }} transition={{ duration: 2 }} >
+          <p>Kevin Arellano</p>
+        </motion.div>
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100, y: 100, rotate: 360 }} transition={{ duration: 2 }} >
+          <img src={logo} className="App-logo" alt="logo" />
+        </motion.div>
       </header>
     </div>
   );
