ba36b96135912cd25bf98895e33df53eeac4d502
Kevin Arellano <Kevin.Arellano94@gmail.com>
2021-03-07 21:39:31 -0500

    .
    .

--- src/App.js
+++ src/App.js
@@ -7,25 +7,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, rotate: 360 }} transition={{ duration: 20 }} >
+        <motion.div initial={{ opacity: 1 }} animate={{ opacity: 1, rotate: 3600 }} transition={{ duration: 300 }} >
           <img src={logo} className="App-logo" alt="logo" />
         </motion.div>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100, y: 100 }} transition={{ duration: 2 }} >
+        
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 3 }} >
+          <p>
+            Explore the world...
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </motion.div>
+
+        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 6 }} >
           <p>Kevin Arellano</p>
-        </motion.div>
-        <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1, x: 100, y: 100, rotate: 360 }} transition={{ duration: 2 }} >
-          <img src={logo} className="App-logo" alt="logo" />
         </motion.div>
       </header>
     </div>
